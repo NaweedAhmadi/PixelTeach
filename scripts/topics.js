0f517f5129dcbd61a3253b6fcf0c55fba4e15a9e
Naweed Ahmadi <naweed.ahmadi2010@gmail.com>
2024-01-06 01:39:44 -0800

    The initial structure of the PixelTeach

---
+++ scripts/topics.js
@@ -0,0 +1,207 @@
+const courseStructure = [
+  {
+    chapterTitle: 'Introduction',
+    lessons: [
+      {
+        lessonTitle: 'An introduction to Apps Script and this course',
+        duration: '3:10',
+        status: 'complete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Templates & Helpful Hints',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'Macros: Our Path into the Apps Script world',
+        duration: '12:40',
+        status: 'complete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Troubleshooting permissions!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'The Apps Script Environment & Hello World',
+        duration: '15:46',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Custom Functions',
+        duration: '11:38',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'November 2021 Update - Please Read!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      }
+    ]
+  },
+  {
+    chapterTitle: 'Fundamentals of Apps Script',
+    lessons: [
+      {
+        lessonTitle: 'An introduction to Apps Script and this course',
+        duration: '3:10',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Templates & Helpful Hints',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'Macros: Our Path into the Apps Script world',
+        duration: '12:40',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Troubleshooting permissions!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'The Apps Script Environment & Hello World',
+        duration: '15:46',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Custom Functions',
+        duration: '11:38',
+        status: 'incomplete',
+        type: 'video'
+      }
+    ]
+  },
+  {
+    chapterTitle: 'Apps Script + Google Sheets',
+    lessons: [
+      {
+        lessonTitle: 'An introduction to Apps Script and this course',
+        duration: '3:10',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Templates & Helpful Hints',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'Macros: Our Path into the Apps Script world',
+        duration: '12:40',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Troubleshooting permissions!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'The Apps Script Environment & Hello World',
+        duration: '15:46',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Custom Functions',
+        duration: '11:38',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'November 2021 Update - Please Read!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      }
+    ]
+  },
+  {
+    chapterTitle: 'Project',
+    lessons: [
+      {
+        lessonTitle: 'An introduction to Apps Script and this course',
+        duration: '3:10',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Templates & Helpful Hints',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'Macros: Our Path into the Apps Script world',
+        duration: '12:40',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Troubleshooting permissions!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'The Apps Script Environment & Hello World',
+        duration: '15:46',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'Custom Functions',
+        duration: '11:38',
+        status: 'incomplete',
+        type: 'video'
+      },
+      {
+        lessonTitle: 'November 2021 Update - Please Read!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'November 2021 Update - Please Read!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      },
+      {
+        lessonTitle: 'November 2021 Update - Please Read!',
+        duration: '',
+        status: 'incomplete',
+        type: 'text'
+      }
+    ]
+  },
+  {
+    chapterTitle: 'Conclusion',
+    lessons: [
+      {
+        lessonTitle: 'An introduction to Apps Script and this course',
+        duration: '3:10',
+        status: 'incomplete',
+        type: 'video'
+      }
+    ]
+  }
+];
