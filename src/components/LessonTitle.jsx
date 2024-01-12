e1ff9daae6b3de800774881f0d144b2ed929ed87
Naweed Ahmadi <naweed.ahmadi2010@gmail.com>
2024-01-11 22:42:15 -0800

    Implemented the useState hook to manage interactive state in the app

--- src/components/LessonTitle.jsx
+++ src/components/LessonTitle.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function LessonTitle(props) {
-    const duration = props.duration === '';
+    const hasDuration = props.duration !== undefined && props.duration !== null && props.duration !== '';
 
-    // function clickHandler() {
-    //     return (
-    //         props.lesson;
-    //     )
-    // }
+    const [isLessonDone, setIsLessonDone] = useState(false);
+    const [startBtnText, setStartBtnText] = useState("Start");
+    const [lessonRowColor, setLessonRowColor] = useState("tr-lesson");
+    const [statusBox, setStatusBox] = useState("fa-regular fa-circle");
+    const [buttonVisibility, setButtonVisibility] = useState(true);
+
+    function clickHandler() {
+        if (isLessonDone) {
+            // Toggle back to initial state
+            setIsLessonDone(false);
+            setButtonVisibility(false);
+            setLessonRowColor("tr-lesson");
+            setStatusBox("fa-solid fa-circle-check");
+        } else {
+            // Toggle to "Done" state
+            setIsLessonDone(true);
+            setStartBtnText("Done");
+            setLessonRowColor("tr-lesson-checked");
+            setStatusBox("fa-solid fa-circle-half-stroke");
+        }
+    }
 
     return (
-        <tr class="tr-lesson">
-            <td class="td-check">
-                <i class="fa-regular fa-circle"></i>
+        <tr className={lessonRowColor}>
+            <td className="td-check">
+                <i className={statusBox}></i>
             </td>
-            <td class="td-lesson">
-                <i class={duration ? "fa-solid fa-align-justify" : "fa-brands fa-youtube"} style={{ width: "30px", fontSize: "20px", verticalAlign: "middle" }}></i>
-                {props.lesson} {duration ? null : "(" + props.duration + ")"}
+            <td className="td-lesson">
+                <i className={hasDuration ? "fa-brands fa-youtube" : "fa-solid fa-align-justify"} style={{ width: "30px", fontSize: "20px", verticalAlign: "middle" }}></i>
+                {props.lesson} {hasDuration ? `(${props.duration})` : null}
             </td>
-            <td onClick class="td-start"><p class="start-btn">Start</p></td>
+            <td onClick={clickHandler} className="td-start">
+                {buttonVisibility ? <p className="start-btn">{startBtnText}</p> : null }
+            </td>
         </tr>
-    )
+    );
 }
 
 export default LessonTitle;
